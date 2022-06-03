273cb5310b66f53ad9b752bab09276f62b968129
nermadie <congtudapchai@gmail.com>
2022-06-04 02:46:43 +0700

    Update Mainform for ADMIN and fix SignInForm

--- PBL3_Main/PBL3/Program.cs
+++ PBL3_Main/PBL3/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using PBL3.GUI.GUI_Login;
 using PBL3.GUI.GUI_Test;
+using PBL3.GUI.GUI_MainForm;
 
 namespace PBL3
 {
@@ -17,7 +18,7 @@ namespace PBL3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
