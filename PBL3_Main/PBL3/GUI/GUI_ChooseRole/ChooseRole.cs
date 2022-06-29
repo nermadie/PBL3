84317390792f78a9909bb6da031aeb30d7665d35
nermadie <nhatminh10b1@gmail.com>
2022-06-30 04:26:24 +0700

    Update animation for ChooseRole 30/6/2022

--- PBL3_Main/PBL3/GUI/GUI_ChooseRole/ChooseRole.cs
+++ PBL3_Main/PBL3/GUI/GUI_ChooseRole/ChooseRole.cs
@@ -12,6 +12,7 @@ using PBL3.GUI.GUI_Alert;
 using PBL3.GUI.GUI_Login.Login_Admin;
 using PBL3.GUI.GUI_Login.Login_Customer;
 using PBL3.GUI.GUI_Login.Login_Employee;
+using Timer = System.Threading.Timer;
 
 namespace PBL3.GUI.GUI_ChooseRole
 {
@@ -60,6 +61,87 @@ namespace PBL3.GUI.GUI_ChooseRole
             la.Show();
             this.Visible = false;
             la.showChooseRole = this.showChooseRole;
+        }
+        //===================//TIMER
+        //Button Customer
+        private void guna2GradientButtonCus_MouseEnter(object sender, EventArgs e)
+        {
+            timerCusLeave.Stop();
+            timerCusEnter.Start();
+        }
+        private void timerCusEnter_Tick(object sender, EventArgs e)
+        {
+            buttonCus.Height += 10;
+            buttonEm.Location = new Point(0, buttonCus.Location.Y + buttonCus.Height + 5);
+            buttonAd.Location = new Point(0, buttonEm.Location.Y + buttonEm.Height + 5);
+            if (buttonCus.Height == buttonCus.MaximumSize.Height)
+                timerCusEnter.Stop();
+        }
+
+        private void guna2GradientButtonCus_MouseLeave(object sender, EventArgs e)
+        {
+            timerCusEnter.Stop();
+            timerCusLeave.Start();
+        }
+
+        private void timerCusLeave_Tick(object sender, EventArgs e)
+        {
+            buttonCus.Height -= 10;
+            buttonEm.Location = new Point(0, buttonCus.Location.Y + buttonCus.Height + 5);
+            buttonAd.Location = new Point(0, buttonEm.Location.Y + buttonEm.Height + 5);
+            if (buttonCus.Height == buttonCus.MinimumSize.Height)
+                timerCusLeave.Stop();
+        }
+        //Button Employee
+        private void buttonEm_MouseEnter(object sender, EventArgs e)
+        {
+            timerEmLeave.Stop();
+            timerEmEnter.Start();
+        }
+        private void timerEmEnter_Tick(object sender, EventArgs e)
+        {
+            buttonEm.Height += 10;
+            buttonAd.Location = new Point(0, buttonEm.Location.Y + buttonEm.Height + 5);
+            if (buttonEm.Height == buttonEm.MaximumSize.Height)
+                timerEmEnter.Stop();
+        }
+        private void buttonEm_MouseLeave(object sender, EventArgs e)
+        {
+            timerEmEnter.Stop();
+            timerEmLeave.Start();
+        }
+        private void timerEmLeave_Tick(object sender, EventArgs e)
+        {
+            buttonEm.Height -= 10;
+            buttonAd.Location = new Point(0, buttonEm.Location.Y + buttonEm.Height + 5);
+            if (buttonEm.Height == buttonEm.MinimumSize.Height)
+                timerEmLeave.Stop();
+        }
+
+        //Button Administrator
+        private void buttonAd_MouseEnter(object sender, EventArgs e)
+        {
+            timerAdLeave.Stop();
+            timerAdEnter.Start();
+        }
+
+        private void timerAdEnter_Tick(object sender, EventArgs e)
+        {
+            buttonAd.Height += 10;
+            if (buttonAd.Height == buttonAd.MaximumSize.Height)
+                timerAdEnter.Stop();
+        }
+
+        private void buttonAd_MouseLeave(object sender, EventArgs e)
+        {
+            timerAdEnter.Stop();
+            timerAdLeave.Start();
+        }
+        private void timerAdLeave_Tick(object sender, EventArgs e)
+        {
+            buttonAd.Height -= 10;
+            if (buttonAd.Height == buttonAd.MinimumSize.Height)
+                timerAdEnter.Stop();
         }
     }
 }
